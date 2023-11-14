bde374b00908da351951ac6715b4fe8cb849438d
nakyoutae <qppk@naver.com>
2023-11-15 00:36:35 +0900

    초안 작성

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // output: 'export',
+    images: {
+        loader: 'imgix',
+        path: 'https://wordscount.kr/images/', // 배포 경로
+    },
+}
 
 module.exports = nextConfig
