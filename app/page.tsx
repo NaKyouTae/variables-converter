d0e8f07671b5e00f382fbce3e9407ceaa153ce88
nakyoutae <qppk@naver.com>
2023-11-21 00:04:33 +0900

    rss 작업 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import {Converter} from "@/app/Converter";
 
 export default function Home() {
--- scripts/generate-rss.ts
+++ scripts/generate-rss.ts
@@ -1,8 +1,7 @@
-var writeFileSync = require('fs');
+var fs = require('fs');
 var RSS = require('rss');
 
-
-const siteUrl = 'https://wordscount.kr/';
+const siteUrl = 'https://variables-converter.kr/';
 
 const master = {
     name: 'kevin',
@@ -27,13 +26,12 @@ const feed = new RSS({
     author: master,
 });
 
-feed.addCategory('Utility');
+feed.item({
+    title: '변수 변환기',
+    description: '기입한 문자를 선택한 유형으로 변경하는 사이트',
+    url: siteUrl,
+});
 
+var xml = feed.xml();
 
-
-// Output: RSS 2.0
-writeFileSync('out/rss.xml', feed.rss2(), 'utf-8');
-// Output: Atom 1.0
-writeFileSync('out/rss-atom.xml', feed.atom1(), 'utf-8');
-// Output: JSON Feed 1.0
-writeFileSync('out/feed.json', feed.json1(), 'utf-8');
+fs.writeFileSync('public/rss.xml', xml, 'utf-8');
